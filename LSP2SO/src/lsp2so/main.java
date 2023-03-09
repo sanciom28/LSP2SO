613a64cb8490e2972e37903c7a5171bd97916a78
Matteo Sancio <matteosancio@gmail.com>
2023-03-09 13:24:02 -0400

    probando ernesto

--- LSP2SO/src/lsp2so/main.java
+++ LSP2SO/src/lsp2so/main.java
@@ -15,7 +15,7 @@ public class main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // PROBANDO MAIN
+        // PROBANDO ERNESTO
     }
     
 }
