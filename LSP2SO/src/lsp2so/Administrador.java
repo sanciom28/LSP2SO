e4b49398c31ba49c773c47d76f1aea40170af68e
Matteo Sancio <matteosancio@gmail.com>
2023-03-16 11:19:09 -0400

    agregando enunciado a demás clases

--- LSP2SO/src/lsp2so/Administrador.java
+++ LSP2SO/src/lsp2so/Administrador.java
@@ -1,7 +1,15 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Administrador (Sistema Operativo): Se encarga de actualizar las colas del sistema
+y de dictarle a la Inteligencia Artificial cuál de las siguientes series se debe
+evisar.
+
+Cada dos ciclos de revisión (cada cuatro series revisadas o dos pares de series),
+el administrador agrega un nueva serie a la cola de su nivel correspondiente. Este
+evento ocurre con una probabilidad del 70%. Se enfatiza que el Administrador opera
+cada vez que la Inteligencia Artificial acaba de terminar de revisar una serie,
+realiza toda la gestión descrita respecto a las colas y finalmente le indica a la
+Inteligencia Artificial que puede empezar a trabajar otra vez con las series
+seleccionadas.
  */
 package lsp2so;
 
