43e9166c8699a701743486d4baf088125cd0d826
Ernesto Lugo <ernesto.lugo@correo.unimet.edu.ve>
2023-03-22 14:42:17 -0400

    bugfixing counters and deleting guide comments

--- LSP2SO/src/lsp2so/Administrador.java
+++ LSP2SO/src/lsp2so/Administrador.java
@@ -1,16 +1,3 @@
-/*
-Administrador (Sistema Operativo): Se encarga de actualizar las colas del sistema
-y de dictarle a la Inteligencia Artificial cuál de las siguientes series se debe
-evisar.
-
-Cada dos ciclos de revisión (cada cuatro series revisadas o dos pares de series),
-el administrador agrega un nueva serie a la cola de su nivel correspondiente. Este
-evento ocurre con una probabilidad del 70%. Se enfatiza que el Administrador opera
-cada vez que la Inteligencia Artificial acaba de terminar de revisar una serie,
-realiza toda la gestión descrita respecto a las colas y finalmente le indica a la
-Inteligencia Artificial que puede empezar a trabajar otra vez con las series
-seleccionadas.
- */
 package lsp2so;
 
 import java.util.Random;
@@ -204,8 +191,7 @@ public class Administrador extends Thread {
                     Interfaz.q1.enQueue(serie);
 
                 } else {
-                    //Se sube el contador y se vuelve a encolar
-                    serie.uppingCounter();
+                    //se vuelve a encolar
                     Interfaz.q2.enQueue(serie);
                 }
             } else {
@@ -233,8 +219,7 @@ public class Administrador extends Thread {
                     Interfaz.q2.enQueue(serie);
 
                 } else {
-                    //Se sube el contador y se vuelve a encolar
-                    serie.uppingCounter();
+                    //se vuelve a encolar
                     Interfaz.q3.enQueue(serie);
                 }
 
@@ -244,7 +229,7 @@ public class Administrador extends Thread {
                 serie.resettingCounter();
                 Interfaz.qFight.enQueue(serie);
             }
-//         
+
         }
     }
 
--- LSP2SO/src/lsp2so/main.java
+++ LSP2SO/src/lsp2so/main.java
@@ -1,23 +1,3 @@
-/*
-- La simulación opera de la siguiente manera: Comienza a ejecutarse y se debe
-crear una serie en cada planta. Una vez allí, comienza a operar la Inteligencia
-Artificial, después el Administrador y así.
-
-- Se debe mostrar en pantalla en todo momento las colas que mantiene el
-Administrador, con los ID de las series y que serie es, cada uno en su respectiva
-cola en el orden en el cual estén, junto con los datos de las series que se
-encuentren en la arena de la Inteligencia Artificial y lo que se encuentra haciendo
-la Inteligencia Artificial en todo momento.
-
-- Se sugiere un control para los tiempos para así variar la velocidad con la que
-rabaja la Inteligencia Artificial y de esta forma poder observar los procesos en
-iempos más rápidos.
-
-- Se pide guardar todos las series que van saliendo al mercado por simulación en
-un TXT, CSV o cualquier alternativa que deseen para almacenar datos y así poder
-sacar un estimado de a qué series les ha ido mejor. De esta forma, HBO Max podrá
-sacar una conclusión.
- */
 package lsp2so;
 
 import lsp2so.Interfaz;
